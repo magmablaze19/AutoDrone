e2377d25009c5ebd0b8e700975529a8f4f786399
Alex Longo <91230868+BlockadeRunner@users.noreply.github.com>
2024-03-06 13:45:02 -0500

    First Commit, basic proof of concept code

---
+++ main_app.py
@@ -0,0 +1,19 @@
+###################################################
+#        Autonomous Drone Main Control Hub        #
+#                                                 #
+#   Co-Authors: Jake Richardson, Alex Passin,     #
+#               and Alex Longo.                   #
+#                                                 #
+#   Date:       3/6/2024                          #
+#                                                 #
+#  Description: TBD                               #
+###################################################
+
+# Imports:
+from djitellopy import tello
+
+
+# Basic Test Code:
+drone = tello.Tello()
+drone.takeoff()
+drone.land()
