752e2f7b48df5cf3c103e6a6c201c30a23a5bc19
Alex Longo <91230868+BlockadeRunner@users.noreply.github.com>
2024-03-25 16:33:34 -0400

    Update CommandResponseLogger.py
    Added documentation for all methods and updated save_log method

--- CommandResponseLogger.py
+++ CommandResponseLogger.py
@@ -22,18 +22,21 @@ class CommandResponseLogger:
     def __init__(self):
         self.log = []
 
+    # Add a command to the event log
     def log_command(self, command: str):
         new_event = self._LogEvent(command, len(self.log))
         self.log.append(new_event)
     
+    # Add a response to the most recent command event in the event log
     def log_reponse(self, response: str):
         self.log[-1].add_response(response)
 
+    # Record a timeout for the most recent command event in the event log
     def log_time_out(self, time_out: bool):
         self.log[-1].update_time_out(time_out)
         
+    # Save the log to a local file for analysis
     def save_log(self):
-        # Open the CommandResponseLog.txt file in write mode
         with open('CommandResponseLog.txt', 'w') as file:
             for event in self.log:
                 # Write each event to the log file
@@ -65,7 +68,7 @@ class CommandResponseLogger:
             if(self.response == None):
                 ret_str_list.append("Response: None\n")
             else:
-                ret_str_list.append("Response: " + self.response + "\n")
+                ret_str_list.append("Response: " + str(self.response) + "\n")
             
             # Copy Command Sent Time
             if(self.start_time == None):
@@ -92,53 +95,63 @@ class CommandResponseLogger:
             ret_str = "".join(ret_str_list) 
             return ret_str
 
-        
+        # Add a reponse to this event
         def add_response(self, response: str):
             self.response = str(response)
             # Calculating total time taken to execute command
             self.end_time = datetime.now()
             self.latency = (self.end_time-self.start_time).total_seconds()
 
+        # Record whether or not a time out occurred for this event
         def update_time_out(self, time_out: bool):
             if(time_out):
                 self.time_out_occured = "Yes"
             else:
                 self.time_out_occured = "No"
         
+        # Check if a response was received for the command sent
         def got_response(self):
             if self.response is None:
                 return False
             else:
                 return True
 
+        # Get the raw response value
         def get_raw_response(self):
             return self.response
-
+        
+        # Process a numeric response
         def numeric_response(self, data: str):
             num_val = ''.join(i for i in data if i.isdigit() or i=='-' or i=='.')
             return num_val
 
+        # Process an integer response
         def int_response(self, data: str):
             return int(self.numeric_response(data))
 
+        # Process a float response
         def float_response(self, data: str):
             return float(self.numeric_response(data))
         
+        # Process an attitude rsponse
         def attitude_response(self):
             raw_att = self.response.split(';')
             att_data = (self.int_response(raw_att[0]), self.int_response(raw_att[1]), self.int_response(raw_att[2]))
             return att_data
         
+        # Process an acceleration response
         def acceleration_response(self):
             raw_acc = self.response.split(';')
             acc_data = (self.float_response(raw_acc[0]), self.float_response(raw_acc[1]), self.float_response(raw_acc[2]))
             return acc_data
 
+        # Process a temperature response
         def temp_response(self):
             raw_temp = self.response.split('~')
             temp = (self.int_response(raw_temp[0]) + self.int_response(raw_temp[1]))/2
             return temp
 
+        # Correctly process response
         def get_response(self):
             if 'attitude?' in self.command:
                 return self.attitude_response()
